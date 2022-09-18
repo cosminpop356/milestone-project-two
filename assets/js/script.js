91e38fdf5573a675b9acbf4dda307d4328653040
cosminpop356 <cosminpop356@gmail.com>
2022-09-18 18:42:54 +0000

    Add computer choice function and incremnet scores functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,16 +4,18 @@ document.addEventListener("DOMContentLoaded", function(){
 
     for (let input of inputs) {
         input.addEventListener("click", function(){
-            if(this.getAttribute("data-type") === 'submit'){
-                alert(`You picked ${'data-type'}`)
+            let playerChoice = this.getAttribute('data-type');
+            if(playerChoice){
+                alert(`You chose ${playerChoice}!`)
             }
         })
     }
+    
 })
 
 
-function runGame() {
-
+function runGame(playerChoice) {
+    
 }
 
 function checkAnswer() {
@@ -21,7 +23,19 @@ function checkAnswer() {
 }
 
 function computerChoice() {
+    let computerRandom = document.getElementsByClassName('computer-choice');
+    let randomNumber = Math.floor(Math.random() * 3);
 
+    if (randomNumber === 0){
+        document.getElementById("computer").innerHTML = computerRandom[0].innerHTML;
+    } else if (randomNumber === 1) {
+        document.getElementById("computer").innerHTML = computerRandom[1].innerHTML;
+    } else if (randomNumber === 2) {
+        document.getElementById("computer").innerHTML = computerRandom[2].innerHTML;
+    } else {
+        alert("An error ocurred! Try again!");
+        throw ("Aborting!");
+    }
 }
 
 function checkRoundWinner() {
@@ -29,11 +43,14 @@ function checkRoundWinner() {
 }
 
 function incrementScorePlayer() {
+    let oldScore = parseInt(document.getElementById('player-score').innerText);
+    document.getElementById('player-score').innerText = ++oldScore;
 
 }
 
 function incrementScoreComputer() {
-
+    let oldScore = parseInt(document.getElementById('computer-score').innerText);
+    document.getElementById('computer-score').innerText = ++oldScore;
 }
 
 function checkGameWinner() {
