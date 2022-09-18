c862524bce5eca27f3c630e04387742b19c9d92f
cosminpop356 <cosminpop356@gmail.com>
2022-09-18 21:43:12 +0000

    Add event listener to listen to user input and make a computer choice at the same time

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,24 +4,34 @@ document.addEventListener("DOMContentLoaded", function(){
 
     for (let input of inputs) {
         input.addEventListener("click", function(){
-            let playerChoice = this.getAttribute('data-type');
-            if(playerChoice){
-                alert(`You chose ${playerChoice}!`)
+            if (this.getAttribute('data-type') === "rock"){
+               document.getElementById('player-area').innerHTML = document.getElementsByClassName('player-choice')[0].innerHTML;
+            } else if(this.getAttribute('data-type') === "paper"){
+                document.getElementById('player-area').innerHTML = document.getElementsByClassName('player-choice')[1].innerHTML;
+            } else if (this.getAttribute('data-type') === "scissors"){
+                document.getElementById('player-area').innerHTML = document.getElementsByClassName('player-choice')[2].innerHTML;
             }
+        computerChoice();
         })
     }
     
 })
 
 
-function runGame(playerChoice) {
+function runGame() {
     
 }
 
-function checkAnswer() {
-
+function playerChoice() {
+    let choiceImage = document.getElementsByClassName('player-choice');
+    document.getElementById('player-area').innerHTML = choiceImage
 }
 
+
+/**
+ * Gets a random number between 0 and 2 and changes innnerHTML of the computer board
+ * with the choice that matches the index of the computer's possible choices.
+ */
 function computerChoice() {
     let computerRandom = document.getElementsByClassName('computer-choice');
     let randomNumber = Math.floor(Math.random() * 3);
@@ -36,18 +46,30 @@ function computerChoice() {
         alert("An error ocurred! Try again!");
         throw ("Aborting!");
     }
+
+    return randomNumber;
+
 }
 
 function checkRoundWinner() {
+    let userChoice = document.getElementsByClassName('easy');
+    let computerChoice = computerChoice();
+
+
 
 }
 
+/**
+ * Adds 1 to the player's score.
+ */
 function incrementScorePlayer() {
     let oldScore = parseInt(document.getElementById('player-score').innerText);
     document.getElementById('player-score').innerText = ++oldScore;
 
 }
-
+/**
+ * Adds 1 to the computer's score.
+ */
 function incrementScoreComputer() {
     let oldScore = parseInt(document.getElementById('computer-score').innerText);
     document.getElementById('computer-score').innerText = ++oldScore;
