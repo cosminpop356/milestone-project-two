98ab9a83cbbcc50ec092d825aa4e4ebee246450a
cosminpop356 <cosminpop356@gmail.com>
2022-10-12 13:50:02 +0000

    Add alerts to notify user about the winner of the round

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,14 +4,53 @@ document.addEventListener("DOMContentLoaded", function(){
 
     for (let input of inputs) {
         input.addEventListener("click", function playerInput(){
+
+            let number = computerChoice();
+            console.log(number);
+
             if (this.getAttribute('data-type') === "rock"){
                document.getElementById('player-area').innerHTML = document.getElementsByClassName('player-choice')[0].innerHTML;
+
+                if(number === 0){
+                    setTimeout(function(){
+                    alert("It's a TIE! Try again.")}, 1000
+                    )
+                } else if (number === 1){
+                    alert("Paper covers Rock. Computer wins!")
+                    incrementScoreComputer();
+                } else {
+                    alert("Rock crushes Scissors. You win!")
+                    incrementScorePlayer();
+                }
+
             } else if(this.getAttribute('data-type') === "paper"){
                 document.getElementById('player-area').innerHTML = document.getElementsByClassName('player-choice')[1].innerHTML;
+
+                if(number === 0){
+                    alert("Paper covers Rock. You win!")
+                    incrementScorePlayer();
+                } else if (number === 1){
+                    alert("It's a TIE! Try again")
+                } else {
+                    alert("Scissors cuts Paper. Computer wins!")
+                    incrementScoreComputer();
+                }
+
             } else if (this.getAttribute('data-type') === "scissors"){
                 document.getElementById('player-area').innerHTML = document.getElementsByClassName('player-choice')[2].innerHTML;
+
+                if(number === 0){
+                    alert("Rock crushes Scissors. Computer wins!")
+                    incrementScoreComputer();
+                } else if (number === 1){
+                    alert("Scissors cuts Paper. You win!")
+                    incrementScorePlayer();
+                } else {
+                    alert("It's a TIE! Try again")
+                }
             }
-        computerChoice();
+        
+        
         })
     }
     
@@ -42,46 +81,7 @@ function computerChoice() {
 }
 
 function checkRoundWinner() {
-    let userInput = document.getElementsByTagName('input').getAttribute("data-type");
-    let computerInput = document.getElementsByClassName('computer-choice');
     
-    for (let i=0; i<computerInput.length; i++){
-
-        if (userInput === "rock" || computerInput[i].getAttribute('data-type') === "rock"){
-            alert("It's a tie! Choose again.");
-
-        } else if (userInput === "rock" || computerInput[i].getAttribute('data-type') === "paper"){
-            alert("Paper covers Rock! Computer wins!");
-            incrementScoreComputer();
-
-        } else if (userInput === "rock" || computerInput[i].getAttribute('data-type') === "scissors"){
-            alert("Rock curshes Scissors! You win!");
-            incrementScorePlayer();
-
-        } else if (userInput === "paper" || computerInput[i].getAttribute('data-type') === "rock"){
-            alert("Paper covers Rock! You win!");
-            incrementScorePlayer();
-
-        } else if (userInput === "paper" || computerInput[i].getAttribute('data-type') === "paper"){
-            alert("It's a tie! Choose again.");
-
-        } else if (userInput === "paper" || computerInput[i].getAttribute('data-type') === "scissors"){
-            alert("Scissors cuts Paper! Computer wins!");
-            incrementScoreComputer();
-
-        } else if (userInput === "scissors" || computerInput[i].getAttribute('data-type') === "rock"){
-            alert("Rock curshes Scissors! Computer wins!");
-            incrementScoreComputer();
-
-        } else if (userInput === "scissors" || computerInput[i].getAttribute('data-type') === "paper"){
-            alert("Scissors cuts Paper! You win!");
-            incrementScorePlayer();
-
-        } else if (userInput === "scissors" || computerInput[i].getAttribute('data-type') === "scissors"){
-            alert("It's a tie! Choose again.");
-        }
-    }
-
 }
 
 /**
