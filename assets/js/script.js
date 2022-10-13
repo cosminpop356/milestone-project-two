bbe40f1511b98bfc3e3e4040a2866b5bba2cc9fa
cosminpop356 <cosminpop356@gmail.com>
2022-10-13 13:47:19 +0000

    Clean up code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+/* Main game function. Gets player input and computer choice to alert user who won. */
 document.addEventListener("DOMContentLoaded", function(){
     let inputs = document.getElementsByTagName('input');
 
@@ -8,6 +9,8 @@ document.addEventListener("DOMContentLoaded", function(){
 
             if (this.getAttribute('data-type') === "rock"){
 
+                /* The code below hides the other choices after user input 
+                and makes them show after 1 second. This code repeats for each choice the user has.*/
                 document.getElementById("player-paper").style.display = 'none';
                 document.getElementById("player-scissors").style.display = 'none'; 
 
@@ -18,25 +21,23 @@ document.addEventListener("DOMContentLoaded", function(){
                
                 if(number === 0){
                     setTimeout(function(){
-                    alert("It's a Tie! Try again.")}, 10);
+                        alert("It's a Tie! Try again.")
+                    }, 10);
                     
-                   
                 } else if (number === 1){
                     setTimeout(function(){
-                    alert("Paper covers Rock. Computer wins!")}, 10);
+                        alert("Paper covers Rock. Computer wins!")
+                    }, 10);
                     incrementScoreComputer();
-                    
-                   
+
                 } else {
                     setTimeout(function(){
-                    alert("Rock crushes Scissors. You win!")}, 10);
+                        alert("Rock crushes Scissors. You win!")
+                    }, 10);
                     incrementScorePlayer();
-                    
-                  
                 }
 
             } else if(this.getAttribute('data-type') === "paper"){
-
                 document.getElementById("player-rock").style.display = 'none';
                 document.getElementById("player-scissors").style.display = 'none'; 
                 
@@ -63,43 +64,40 @@ document.addEventListener("DOMContentLoaded", function(){
                 }
 
             } else if (this.getAttribute('data-type') === "scissors"){
-
                 document.getElementById("player-paper").style.display = 'none';
                 document.getElementById("player-rock").style.display = 'none'; 
                 
                 setTimeout(function(){
-                    document.getElementById("player-paper").style.display = 'block'
+                    document.getElementById("player-paper").style.display = 'block';
                     document.getElementById("player-rock").style.display = 'block'; 
                 }, 1000);
 
                 if(number === 0){
                     setTimeout(function(){
-                    alert("Rock crushes Scissors. Computer wins!")}, 10);
+                        alert("Rock crushes Scissors. Computer wins!")
+                    }, 10);
                     incrementScoreComputer();
                     
                 } else if (number === 1){
                     setTimeout(function(){
-                    alert("Scissors cuts Paper. You win!")}, 10);
+                        alert("Scissors cuts Paper. You win!")
+                    }, 10);
                     incrementScorePlayer();
                    
                 } else {
                     setTimeout(function(){
-                    alert("It's a TIE! Try again")}, 10);
-                   
+                        alert("It's a TIE! Try again")
+                    }, 10);  
                 }
             }
-        
             checkGameWinner();
         })
     }
-    
-    
-
 })
 
 /**
- * Gets a random number between 0 and 2 and changes innnerHTML of the computer board
- * with the choice that matches the index of the computer's possible choices.
+ * Gets a random number that matches a choice and
+ * hides the other choices that don't match the random number.
  */
 function computerChoice() {
     
@@ -111,7 +109,7 @@ function computerChoice() {
         document.getElementById("computer-scissors").style.display = 'none'; 
 
         setTimeout(function(){
-            document.getElementById("computer-paper").style.display = 'block'
+            document.getElementById("computer-paper").style.display = 'block';
             document.getElementById("computer-scissors").style.display = 'block'; 
         }, 1000);
 
@@ -121,7 +119,7 @@ function computerChoice() {
         document.getElementById("computer-scissors").style.display = 'none'; 
                 
         setTimeout(function(){
-            document.getElementById("computer-rock").style.display = 'block'
+            document.getElementById("computer-rock").style.display = 'block';
             document.getElementById("computer-scissors").style.display = 'block'; 
         }, 1000);
 
@@ -131,7 +129,7 @@ function computerChoice() {
         document.getElementById("computer-rock").style.display = 'none'; 
                 
         setTimeout(function(){
-            document.getElementById("computer-paper").style.display = 'block'
+            document.getElementById("computer-paper").style.display = 'block';
             document.getElementById("computer-rock").style.display = 'block'; 
         }, 1000);
     } 
@@ -140,7 +138,7 @@ function computerChoice() {
 }
 
 /**
- * Adds 1 to the player's score.
+ * Gets the player score from DOM and adds 1 to it.
  */
 function incrementScorePlayer() {
     let oldScore = parseInt(document.getElementById('player-score-number').innerText);
@@ -148,30 +146,36 @@ function incrementScorePlayer() {
 }
 
 /**
- * Adds 1 to the computer's score.
+ * Gets the computer score from DOM and adds 1 to it.
  */
 function incrementScoreComputer() {
     let oldScore = parseInt(document.getElementById('computer-score-number').innerText);
     document.getElementById('computer-score-number').innerText = ++oldScore;
 }
 
+
+/**
+ * Alerts user when a score reached 3 points and reloads page.
+ */
 function checkGameWinner(){
     let playerScore = parseInt(document.getElementById('player-score-number').innerText);
     let computerScore = parseInt(document.getElementById('computer-score-number').innerText);
 
     if (playerScore === 3){
         setTimeout(function(){
-            alert("You reached 3 points. You win!")
+            alert("You reached 3 points. You win!");
+            location.reload();
         }, 10);
+
         
-        location.reload();
         
     } else if (computerScore === 3){
         setTimeout(function(){
-            alert("Computer reached 3 points. You lose!")
+            alert("Computer reached 3 points. You lose!");
+            location.reload();
         }, 10);
 
-        location.reload();
+        
         
     }
 }
